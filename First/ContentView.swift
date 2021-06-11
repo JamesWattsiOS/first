14f82bf2307d1912ca22e241d61b88f59f068313
JamesWattsiOS <james@hire-programmers.com>
2021-06-11 06:30:05 +0600

    Initial Commit

---
+++ First/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First
+//
+//  Created by MD Tanvir Alam on 11/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by MD Tanvir Alam on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
